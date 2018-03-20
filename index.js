921b1750bd4970ecf4f972356b37e8204810d5e0
Alexis Russell <alexissrussell@users.noreply.github.com>
2018-03-20 01:36:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const server = require("http").Server();
 const port = process.env.PORT || 10000;
 
+Access-Control-Allow-Origin: http:worldwebapp.herokuapp.com/
+
 
 var io = require("socket.io")(server);
 
